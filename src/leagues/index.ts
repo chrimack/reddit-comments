979c98cdc386d57dc93fd5e0072b788e23f2b3d4
Chris McClelland <clm727@gmail.com>
2025-07-02 17:22:07 -0400

    refactor: remove leagues module; eliminate unused league utilities and metadata

--- src/leagues/index.ts
+++
@@ -1,3 +0,0 @@
-export { LeagueUtils } from './league.utils.ts';
-export { LeagueMetaMap, Leagues } from './meta.ts';
-export type { League, LeagueMeta } from './types.ts';
--- src/leagues/types.ts
+++
@@ -1,7 +0,0 @@
-export type League = 'MLB' | 'WNBA';
-
-export interface LeagueMeta {
-  key: League;
-  title: string;
-  subreddit: string;
-}
